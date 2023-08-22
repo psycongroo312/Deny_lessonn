a2287484bff78d1748f1953c8c0356b4dcd0b01b
Deny <deniphoto@mail.ru>
2023-08-22 15:29:07 +0300

    lessonnn_8

---
+++ app.js
@@ -0,0 +1,33 @@
+const numbers = [];
+
+for (let i = 0; i <= 1000; i++) {
+    if(isEven(i)){
+        numbers.push(i)
+    }
+}
+
+function isEven(value){
+    return value % 2 === 0;
+}
+
+
+const array = [];
+
+for (let i = 0; i < numbers.length; i++ ){
+    if(i % 3 === 0){;
+    array.push(i)
+    };
+}
+
+
+const stepen = array.map(function(value, index){
+    return value ** 2;
+   
+});
+
+
+const getIndex = stepen.indexOf(324);
+
+console.log(getIndex);
+
+// где код?
